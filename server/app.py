a245a5eb22b70202dda1ba492c8774af15b53401
Aaron Levy <aaronlevypc21@gmail.com>
2024-03-01 12:37:44 -0500

    nothing

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_restful import Api, Resource
 from models import db, User, Game, Rating
 
 # local imports
-from config import create_app, db
+from config import create_app, db, api
 
 # Add your model imports
 
@@ -118,6 +118,7 @@ def add_ratings():
     except:
         return {'error': "Invalid Data"}
     
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
